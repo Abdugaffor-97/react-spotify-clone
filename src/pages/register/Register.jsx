101fec07c3d8538167a563a43eebda906de7d5ef
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-02-20 17:48:55 +0500

    Style updated login and register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./style.scss";
 import ContinueWith from "../../components/button/ContinueWith";
 import { Container } from "react-bootstrap";
+import SpotifyImg from "../../components/styled_components/SpotifyImg";
 
 const Register = () => {
   const [email, setEmail] = useState("");
@@ -28,138 +29,137 @@ const Register = () => {
   };
 
   return (
-    <Container style={{ maxWidth: "550px" }}>
-      {/* <div className="container" style={{ maxWidth: "100%" }}> */}
-          <img
-            style={{ maxWidth: "300px" }}
-            src={process.env.PUBLIC_URL + "Assets/Spotify_Logo_RGB_White.svg"}
-            alt="Spotify Logo White"
-          />
-        <hr />
-      {/* </div> */}
-      <ContinueWith
-        title="Sign up with your email address"
-        className="btn btn-lg btn-block btn-email"
-      />
-      <form onSubmit={submitUser}>
-        <div className="form-group">
-          <label htmlFor="EnterEmail">What's your email?</label>
-          <input
-            type="email"
-            className="form-control"
-            id="EnterEmail"
-            placeholder="Enter your email."
-            aria-describedby="emailHelp"
-            name="Email1"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="EnterPassword">Create a password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Create a password."
-            id="EnterPassword"
-            name="password1"
-            value={password1}
-            onChange={(e) => setPassword1(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="ConfirmPassword">Confirm your password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Confirm your password."
-            id="ConfirmPassword"
-            name="password2"
-            value={password2}
-            onChange={(e) => setPassword2(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="ProfileName">What should we call you?</label>
-          <input
-            type="text"
-            className="form-control"
-            id="ProfileName"
-            placeholder="Enter a profile name."
-            aria-describedby="emailHelp"
-            name="userid"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <p style={{ fontSize: "small" }}>This appears on your profile.</p>
-        </div>
-        <label>What's your gender?</label>
-        <div className="form-group">
-          <div className="custom-control custom-radio custom-control-inline">
+    <div id="register" className="pt-5">
+      <SpotifyImg />
+      <hr />
+      <Container style={{ maxWidth: "550px" }}>
+        <ContinueWith
+          title="Sign up with your email address"
+          className="btn btn-lg btn-block btn-email"
+        />
+        <form onSubmit={submitUser}>
+          <div className="form-group">
+            <label htmlFor="EnterEmail">What's your email?</label>
             <input
-              type="radio"
-              id="customRadioInline1"
-              name="customRadioInline1"
-              className="custom-control-input"
+              type="email"
+              className="form-control"
+              id="EnterEmail"
+              placeholder="Enter your email."
+              aria-describedby="emailHelp"
+              name="Email1"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-            <label
-              className="custom-control-label"
-              htmlFor="customRadioInline1"
-            >
-              Male
+          </div>
+          <div className="form-group">
+            <label htmlFor="EnterPassword">Create a password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Create a password."
+              id="EnterPassword"
+              name="password1"
+              value={password1}
+              onChange={(e) => setPassword1(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="ConfirmPassword">Confirm your password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Confirm your password."
+              id="ConfirmPassword"
+              name="password2"
+              value={password2}
+              onChange={(e) => setPassword2(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="ProfileName">What should we call you?</label>
+            <input
+              type="text"
+              className="form-control"
+              id="ProfileName"
+              placeholder="Enter a profile name."
+              aria-describedby="emailHelp"
+              name="userid"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <p style={{ fontSize: "small" }}>This appears on your profile.</p>
+          </div>
+          <label>What's your gender?</label>
+          <div className="form-group">
+            <div className="custom-control custom-radio custom-control-inline">
+              <input
+                type="radio"
+                id="customRadioInline1"
+                name="customRadioInline1"
+                className="custom-control-input"
+              />
+              <label
+                className="custom-control-label"
+                htmlFor="customRadioInline1"
+              >
+                Male
+              </label>
+            </div>
+            <div className="custom-control custom-radio custom-control-inline">
+              <input
+                type="radio"
+                id="customRadioInline2"
+                name="customRadioInline1"
+                className="custom-control-input"
+              />
+              <label
+                className="custom-control-label"
+                htmlFor="customRadioInline2"
+              >
+                Female
+              </label>
+            </div>
+            <div className="custom-control custom-radio custom-control-inline">
+              <input
+                type="radio"
+                id="customRadioInline3"
+                name="customRadioInline1"
+                className="custom-control-input"
+              />
+              <label
+                className="custom-control-label"
+                htmlFor="customRadioInline3"
+              >
+                Non-binary
+              </label>
+            </div>
+          </div>
+          <div className="custom-control custom-checkbox mb-3">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              I agree to the <a href="#aaaa">Spotify terms & conditions</a> and{" "}
+              <a href="#aaaa">Privacy Policy</a>.
             </label>
           </div>
-          <div className="custom-control custom-radio custom-control-inline">
-            <input
-              type="radio"
-              id="customRadioInline2"
-              name="customRadioInline1"
-              className="custom-control-input"
-            />
-            <label
-              className="custom-control-label"
-              htmlFor="customRadioInline2"
+          <div>
+            <Link
+              className="btn btn-success btn-lg"
+              style={{ width: "500px" }}
+              to="/"
             >
-              Female
-            </label>
+              Sign up
+            </Link>
           </div>
-          <div className="custom-control custom-radio custom-control-inline">
-            <input
-              type="radio"
-              id="customRadioInline3"
-              name="customRadioInline1"
-              className="custom-control-input"
-            />
-            <label
-              className="custom-control-label"
-              htmlFor="customRadioInline3"
-            >
-              Non-binary
-            </label>
-          </div>
-        </div>
-        <div className="custom-control custom-checkbox mb-3">
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="customCheck1"
-          />
-          <label className="custom-control-label" htmlFor="customCheck1">
-            I agree to the <a href="#aaaa">Spotify terms & conditions</a> and{" "}
-            <a href="#aaaa">Privacy Policy</a>.
-          </label>
-        </div>
-        <div>
-          <Link
-            className="btn btn-success btn-lg"
-            style={{ width: "500px" }}
-            to="/"
-          >
-            Sign up
-          </Link>
-        </div>
-      </form>
-    </Container>
+        </form>
+        <p className="mt-3">
+          Have an account? <Link to="/login">Log in.</Link>
+        </p>
+      </Container>
+    </div>
   );
 };
 
