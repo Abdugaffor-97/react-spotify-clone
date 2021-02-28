320e86a0dfb29b66fbdd42d5b557f9a82202255b
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-02-28 17:53:31 +0500

    Styles updated

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
 import { data } from "./borwse";
 import "./styles.scss";
 
 const Search = () => {
   return (
     <div id="list">
-      <Col>
-        <Row xs={2} sm={3} md={4} lg={6} xl={7}>
-          {data.items.map((item, idx) => (
-            <div key={idx} id="item">
-              <h3>{item.name}</h3>
-              <img src={item.icons[0].url} alt={item.name} />
-            </div>
-          ))}
-        </Row>
-      </Col>
+      {data.items.map((item, idx) => (
+        <div key={idx} id="item">
+          <h5>{item.name}</h5>
+          <img src={item.icons[0].url} alt={item.name} />
+        </div>
+      ))}
     </div>
   );
 };
