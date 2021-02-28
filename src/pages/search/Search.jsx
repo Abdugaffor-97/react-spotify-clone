cc16995323ea7b4ae19fdf338a0f58f4f53b9ba6
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-02-28 16:30:33 +0500

    Imp search

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { data } from "./borwse";
+import "./styles.scss";
 
 const Search = () => {
-  return <h3>Browse all </h3>;
+  return (
+    <div id="list">
+      <Col>
+        <Row xs={2} sm={3} md={4} lg={6} xl={7}>
+          {data.items.map((item, idx) => (
+            <div key={idx} id="item">
+              <h3>{item.name}</h3>
+              <img src={item.icons[0].url} alt={item.name} />
+            </div>
+          ))}
+        </Row>
+      </Col>
+    </div>
+  );
 };
 
 export default Search;
---
+++ src/pages/search/borwse.js
@@ -0,0 +1,364 @@
+export const genres = [
+  "acoustic",
+  "afrobeat",
+  "alt-rock",
+  "alternative",
+  "ambient",
+  "anime",
+  "black-metal",
+  "bluegrass",
+  "blues",
+  "bossanova",
+  "brazil",
+  "breakbeat",
+  "british",
+  "cantopop",
+  "chicago-house",
+  "children",
+  "chill",
+  "classical",
+  "club",
+  "comedy",
+  "country",
+  "dance",
+  "dancehall",
+  "death-metal",
+  "deep-house",
+  "detroit-techno",
+  "disco",
+  "disney",
+  "drum-and-bass",
+  "dub",
+  "dubstep",
+  "edm",
+  "electro",
+  "electronic",
+  "emo",
+  "folk",
+  "forro",
+  "french",
+  "funk",
+  "garage",
+  "german",
+  "gospel",
+  "goth",
+  "grindcore",
+  "groove",
+  "grunge",
+  "guitar",
+  "happy",
+  "hard-rock",
+  "hardcore",
+  "hardstyle",
+  "heavy-metal",
+  "hip-hop",
+  "holidays",
+  "honky-tonk",
+  "house",
+  "idm",
+  "indian",
+  "indie",
+  "indie-pop",
+  "industrial",
+  "iranian",
+  "j-dance",
+  "j-idol",
+  "j-pop",
+  "j-rock",
+  "jazz",
+  "k-pop",
+  "kids",
+  "latin",
+  "latino",
+  "malay",
+  "mandopop",
+  "metal",
+  "metal-misc",
+  "metalcore",
+  "minimal-techno",
+  "movies",
+  "mpb",
+  "new-age",
+  "new-release",
+  "opera",
+  "pagode",
+  "party",
+  "philippines-opm",
+  "piano",
+  "pop",
+  "pop-film",
+  "post-dubstep",
+  "power-pop",
+  "progressive-house",
+  "psych-rock",
+  "punk",
+  "punk-rock",
+  "r-n-b",
+  "rainy-day",
+  "reggae",
+  "reggaeton",
+  "road-trip",
+  "rock",
+  "rock-n-roll",
+  "rockabilly",
+  "romance",
+  "sad",
+  "salsa",
+  "samba",
+  "sertanejo",
+  "show-tunes",
+  "singer-songwriter",
+  "ska",
+  "sleep",
+  "songwriter",
+  "soul",
+  "soundtracks",
+  "spanish",
+  "study",
+  "summer",
+  "swedish",
+  "synth-pop",
+  "tango",
+  "techno",
+  "trance",
+  "trip-hop",
+  "turkish",
+  "work-out",
+  "world-music",
+];
+
+export const data = {
+  items: [
+    {
+      icons: [
+        {
+          height: 275,
+          url:
+            "https://t.scdn.co/media/derived/toplists_11160599e6a04ac5d6f2757f5511778f_0_0_275_275.jpg",
+          width: 275,
+        },
+      ],
+      id: "toplists",
+      name: "Topplistor",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url:
+            "https://t.scdn.co/media/derived/pop-274x274_447148649685019f5e2a03a39e78ba52_0_0_274_274.jpg",
+          width: 274,
+        },
+      ],
+      id: "pop",
+      name: "Pop",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url:
+            "https://t.scdn.co/media/original/hip-274_0a661854d61e29eace5fe63f73495e68_274x274.jpg",
+          width: 274,
+        },
+      ],
+      id: "hiphop",
+      name: "Hip Hop",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url:
+            "https://t.scdn.co/media/original/mood-274x274_976986a31ac8c49794cbdc7246fd5ad7_274x274.jpg",
+          width: 274,
+        },
+      ],
+      id: "mood",
+      name: "Humör",
+    },
+    {
+      icons: [
+        {
+          height: null,
+          url: "https://t.scdn.co/images/fe06caf056474bc58862591cd60b57fc.jpeg",
+          width: null,
+        },
+      ],
+      id: "indie_alt",
+      name: "Indie",
+    },
+    {
+      icons: [
+        {
+          height: null,
+          url: "https://t.scdn.co/images/04da469dd7be4dab96659aa1fa9f0ac9.jpeg",
+          width: null,
+        },
+      ],
+      id: "at_home",
+      name: "Hemma",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url:
+            "https://t.scdn.co/media/derived/chill-274x274_4c46374f007813dd10b37e8d8fd35b4b_0_0_274_274.jpg",
+          width: 274,
+        },
+      ],
+      id: "chill",
+      name: "Chill",
+    },
+    {
+      icons: [
+        {
+          height: null,
+          url: "https://t.scdn.co/images/2adf91e7179a4e098834d630c74824b1.jpeg",
+          width: null,
+        },
+      ],
+      id: "wellness",
+      name: "Välmående",
+    },
+    {
+      icons: [
+        {
+          height: null,
+          url: "https://t.scdn.co/images/b611cf5145764c64b80e91ccd5f357c8",
+          width: null,
+        },
+      ],
+      id: "decades",
+      name: "Årtionden",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url:
+            "https://t.scdn.co/media/derived/r-b-274x274_fd56efa72f4f63764b011b68121581d8_0_0_274_274.jpg",
+          width: 274,
+        },
+      ],
+      id: "rnb",
+      name: "R&B",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url:
+            "https://t.scdn.co/media/derived/edm-274x274_0ef612604200a9c14995432994455a6d_0_0_274_274.jpg",
+          width: 274,
+        },
+      ],
+      id: "edm_dance",
+      name: "Dance/electronic",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url:
+            "https://t.scdn.co/media/derived/rock_9ce79e0a4ef901bbd10494f5b855d3cc_0_0_274_274.jpg",
+          width: 274,
+        },
+      ],
+      id: "rock",
+      name: "Rock",
+    },
+    {
+      icons: [
+        {
+          height: null,
+          url: "https://t.scdn.co/media/links/workout-274x274.jpg",
+          width: null,
+        },
+      ],
+      id: "workout",
+      name: "Träning",
+    },
+    {
+      icons: [
+        {
+          height: null,
+          url: "https://t.scdn.co/images/781e4ef90e4d48e889d9c15bc35fdec6",
+          width: null,
+        },
+      ],
+      id: "blackhistorymonth",
+      name: "Black History Is Now",
+    },
+    {
+      icons: [
+        {
+          height: null,
+          url: "https://t.scdn.co/images/4ebb98ae04d34698a3a1ac4252c69ac6.jpeg",
+          width: null,
+        },
+      ],
+      id: "family",
+      name: "Barn och familj",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url:
+            "https://t.scdn.co/media/original/metal_27c921443fd0a5ba95b1b2c2ae654b2b_274x274.jpg",
+          width: 274,
+        },
+      ],
+      id: "metal",
+      name: "Metal",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url:
+            "https://t.scdn.co/media/original/dinner_1b6506abba0ba52c54e6d695c8571078_274x274.jpg",
+          width: 274,
+        },
+      ],
+      id: "dinner",
+      name: "Matlagning och middag",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url:
+            "https://t.scdn.co/media/original/genre-images-square-274x274_5e50d72b846a198fcd2ca9b3aef5f0c8_274x274.jpg",
+          width: 274,
+        },
+      ],
+      id: "focus",
+      name: "Fokus",
+    },
+    {
+      icons: [
+        {
+          height: null,
+          url: "https://t.scdn.co/images/b4182906bf244b4994805084c057e9ee.jpeg",
+          width: null,
+        },
+      ],
+      id: "thirdparty",
+      name: "Trendsättare",
+    },
+    {
+      icons: [
+        {
+          height: 274,
+          url: "https://t.scdn.co/media/links/partyicon_274x274.jpg",
+          width: 274,
+        },
+      ],
+      id: "party",
+      name: "Party",
+    },
+  ],
+};
