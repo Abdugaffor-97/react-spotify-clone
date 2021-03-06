6c37f9cef53946f4d70895756a80dd074da2c3a9
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-03-06 15:55:44 +0500

    Custom hok created for tggling songs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ProgressBar } from "react-bootstrap";
-import { IoPlayCircleOutline, IoRepeat } from "react-icons/io5";
+import { IoRepeat } from "react-icons/io5";
 import { IoIosShuffle } from "react-icons/io";
 import { BiSkipNext, BiSkipPrevious } from "react-icons/bi";
 import { CgPlayList } from "react-icons/cg";
@@ -8,11 +8,16 @@ import { MdDevices } from "react-icons/md";
 import { BsFillVolumeUpFill } from "react-icons/bs";
 import "./style.scss";
 import { useSelector } from "react-redux";
+import { AiOutlinePause } from "react-icons/ai";
+import { IoPlayOutline } from "react-icons/io5";
+import useTogglePlay from "../../hooks/useTogglePlay";
 
 const Footer = () => {
-  const { avatar, artist_name, title } = useSelector(
+  const { avatar, artist_name, title, isPlaying, preview } = useSelector(
     (state) => state.currentTrack
   );
+
+  const togglePlay = useTogglePlay();
 
   return (
     <footer className="text-center">
@@ -33,19 +38,24 @@ const Footer = () => {
             <div>
               <IoIosShuffle />
               <BiSkipPrevious />
-              <IoPlayCircleOutline />
+              {isPlaying ? (
+                <IoPlayOutline onClick={togglePlay} />
+              ) : (
+                <AiOutlinePause onClick={togglePlay} />
+              )}
+
               <BiSkipNext />
               <IoRepeat />
             </div>
             <div className="player-center">
               <div className="d-flex justify-content-between align-items-center">
                 <div>
-                  <b className="text-white"> 0:00 </b>
+                  <b className="text-white mr-3"> 0:00 </b>
                 </div>
                 <ProgressBar now={90} />
-                <audio id="audio"></audio>
+                <audio src={preview}></audio>
                 <div>
-                  <b className="text-white"> 10:30 </b>
+                  <b className="text-white ml-2"> 0:30 </b>
                 </div>
               </div>
             </div>
---
+++ src/hooks/useTogglePlay.js
@@ -0,0 +1,18 @@
+import { useDispatch, useSelector } from "react-redux";
+import { togglePlaySong } from "../actions/currentTrackActions";
+
+const useTogglePlay = () => {
+  const dispatch = useDispatch();
+
+  const { isPlaying } = useSelector((state) => state.currentTrack);
+
+  const togglePlay = () => {
+    const audio = document.querySelector("audio");
+    dispatch(togglePlaySong());
+    isPlaying ? audio.play() : audio.pause();
+  };
+
+  return togglePlay;
+};
+
+export default useTogglePlay;
